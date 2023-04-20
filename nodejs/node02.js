787d906707cb0128528b89ceb3b22f8374bccdca
Artur Bardziński <artur.bardzinski@icloud.com>
2023-04-20 18:59:08 +0200

    aadd files

---
+++ nodejs/node02.js
@@ -0,0 +1,13 @@
+var http = require("http");
+var url = require("url");
+
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.write("req.url: " + req.url + "<br>");
+    let parsedUrl = url.parse(req.url, true);
+    res.write("parsedUrl.pathname: " + parsedUrl.pathname + "<br>");
+    res.write("parsedUrl.query: " + JSON.stringify(parsedUr1.query));
+    res.end();
+  })
+  .listen(8080);
